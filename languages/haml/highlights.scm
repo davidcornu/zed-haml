f535be24c5eb429fbdd8e05b4aaa6c659fd2d577
David Cornu <me@davidcornu.com>
2024-09-13 11:51:09 -0400

    Initial commit

---
+++ languages/haml/highlights.scm
@@ -0,0 +1,6 @@
+(text_content) @string
+(name) @type
+(ruby_block_run "-" @keyword)
+(ruby_block_output "=" @keyword)
+(class) @string
+(id) @string
---
+++ languages/haml/injections.scm
@@ -0,0 +1,5 @@
+((ruby_block_run) @content
+    (#set! language ruby))
+
+((ruby_block_output) @content
+    (#set! language ruby))
